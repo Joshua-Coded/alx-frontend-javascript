853ec43d727df5b1abb5ae774f79a737fcddcaa0
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-17 10:22:37 -0700

    task

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
     const task = true;
+    const task2 = false;
+  }
 
-
-    if(trueOrFalse){
-	const task = true;
-	const task2 = false;
-    }
-
-    return [task, task2];
+  return [task, task2];
 }
