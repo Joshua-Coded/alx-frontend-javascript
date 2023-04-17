1c45ac4d155a3e125d586feeed53e08430ff97c3
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-17 12:17:44 -0700

    task 101

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,3 @@
- export default function iterateThroughObject(reportWithIterator) {	
-   return [...reportWithIterator].join(' | ');	 
- }
+export default function iterateThroughObject(reportWithIterator) {
+  return [...reportWithIterator].join(' | ');
+}
