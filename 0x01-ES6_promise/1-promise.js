16ce58377a0e9ce1c2290e543de13e787d31595e
Joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-19 11:56:49 -0700

    1-promise.js

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -3,7 +3,7 @@ export default function getFullResponseFromAPI(success) {
     if (sucess) {
 	resolve({status: 200, body: 'Success' });
     } else {
-      reject(new Error('The api is not working!'));
+      reject(new Error('The fake API is not working currently'));
       }
   });
 }
